3f4c80d8ede7c04429d28b19409563b28194ca51
kamakiri01 <kamakiri01@users.noreply.github.com>
2022-08-25 13:36:17 +0900

    bump latest version

--- src/app/app.ts
+++ src/app/app.ts
@@ -210,8 +210,9 @@ function getNotificationInfo(context: AppContext, latestLog: ActivityLog[]) {
 }
 
 function getLog(filePath: string): ActivityLog[] | null {
-    return parseVRChatLog(
+    const log = parseVRChatLog(
         fs.readFileSync(path.resolve(filePath), "utf8"), false);
+    return log ? log.activityLogList : null;
 }
 
 // 自身の退室時か、leaveユーザ名リストに自身のdisplayNameが含まれる場合は通知しない
